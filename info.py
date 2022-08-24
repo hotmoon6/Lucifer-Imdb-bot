58f80fe6aad1e45ece4e2e409bbb84dd63eada44
Hotmoon <aravind1333.av@gmail.com>
2022-08-24 20:06:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,17 +33,27 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Dingdi")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+DATABASE_NAME = environ.get('DATABASE_NAME', "SadieBot")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Mindflayers_Mirror')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'mizotelegram')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'arvinxoxo')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), True)
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "<code>{file_name}</code>\n\n<b>Size:</b> {file_size}\n\n{file_caption}\n\n<b>[© TVSeries & Movie Studio](https://t.me/joinchat/prE6ALN6x2hkY2E1)</b>")
-IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<b>🎬 Title:</b> <a href={url}>{title}</a>\n<b>📺 Type:</b> {kind}\n<b>📆 Release:</b> <a href={url}/releaseinfo>{release_date}</a>\n<b>🌟 Rating:</b> <a href={url}/ratings>{rating} / 10</a>\n(based on <code>{votes}</code> user ratings.)\n\n<b>📀 Runtime:</b> <code>{runtime} minutes</code>\n<b>🎭 Genres:</b> {genres}\n\n<b>☀️ Languages:</b> {languages}\n<b>🎛 Countries:</b> {countries}\n<b>🎥 Director:</b> {director}\n<b>📝 Writers:</b> {writer}\n\n<b><a href='https://t.me/joinchat/prE6ALN6x2hkY2E1'>© TVSeries & Movie Studio</a></b>")
+IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", '''<b>🎬 Title:</b> <a href={url}>{title}</a>
+<b>⭐ Rating:</b> <i>{rating} / 10</i>
+(<code>based on {votes} user ratings.</code>)
+<b>💿 Runtime:</b> <code>{runtime} Minutes</code> 
+<b>📆 Release Info:</b> {release_date}
+<b>🎭 Genere:</b> {genres}
+<b>☀️ Language:</b> {languages}
+<b>🌎 Country of Origin:</b> {countries}
+<b>🎥 Storyline:</b>  <i>{plot}</i>
+
+⚠️ Powered by <b>#Mindflayers_Mirror</b>''')
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "False"), False)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
