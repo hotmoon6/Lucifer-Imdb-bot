6d5f30f139f161435e2819432ac88e6e338360ce
Hotmoon <aravind1333.av@gmail.com>
2022-08-25 20:12:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/hotmoon6/Lucifer-imdbbot.git /IMDb-Movie-Bot
+git clone -b master https://github.com/hotmoon6/Lucifer-Imdb-bot.git /IMDb-Movie-Bot
 cd /IMDb-Movie-Bot
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
